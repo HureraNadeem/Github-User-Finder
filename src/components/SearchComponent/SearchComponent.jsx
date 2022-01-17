66f2e9c8674df475d4c5fb788951387c6f2eb4c5
Hurera Nadeem <muhammadhureran8@gmail.com>
2022-01-17 14:03:41 +0500

    Enter to search implemented

--- src/components/SearchComponent/SearchComponent.jsx
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -7,11 +7,18 @@ function SearchComponent(props) {
     const sendData = () => {
         props.setUsername(userName);
     }
+
+    const keyPressed = (event) => {
+        if (event.key === 'Enter') {
+            sendData();
+        }
+    }
+
     return (
         <>
             <div className='search_div d-flex justify-content-center'>
-                <input className='search_bar' placeholder='Enter Username' type="text" onChange={(e) => { setUsername(e.target.value) }} />  
-                <button className='btn btn-light search_button' onClick={sendData}>Search</button>
+                <input className='search_bar' placeholder='Enter Username' type="text" onKeyPress={keyPressed} onChange={(e) => { setUsername(e.target.value) }} />
+                <button className='btn btn-light search_button' onClick={sendData}  >Search</button>
             </div>
         </>
     )
